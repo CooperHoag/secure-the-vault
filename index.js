4b25a2df39ac4ffaa63da5be76090d355b0cc000
CooperHoag <cooperhoag@gmail.com>
2025-05-14 12:37:07 -0700

    Add JavaScript let and const's

---
+++ index.js
@@ -0,0 +1,16 @@
+let code1;
+code1 = 7 + 3
+
+const code2 = (50 - 10) / 1;
+
+const code3 = 100 % 61;
+
+const message = `The vault has been secured. The combination is:`;
+
+const codeA = code1 + "-" + code2 + "-" + code3;
+
+const codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
